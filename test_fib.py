fdb0b1cd590f123a49da217476b713187c849ce2
Kian Nassre <knassre@macbook-pro-132.wifi.local.cmu.edu>
2022-08-31 12:57:02 -0400

    Extra tests

--- test_fib.py
+++ test_fib.py
@@ -1,4 +1,5 @@
 from fib import fibonacci
+import pytest
 
 # Test cases for the fibonacci function
 
@@ -13,3 +14,19 @@ def test_21st_fibonacci():
 
 def test_negative_fibonacci():
     assert(fibonacci(-1) == None)
+
+@pytest.mark.parametrize(
+    "n, answer",
+    [
+        (2, 1),
+        (3, 2),
+        (4, 3),
+        (5, 5),
+        (6, 8),
+        (7, 13),
+        (8, 21),
+        (9, 34),
+    ]
+)
+def test_fib_various(n, answer):
+    assert(fibonacci(n) == answer)
