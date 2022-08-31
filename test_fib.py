00a258af869f412c3e2a0a660208b05580d77caf
Kian Nassre <knassre@macbook-pro-132.wifi.local.cmu.edu>
2022-08-31 13:08:46 -0400

    Renaming tests

--- test_fib.py
+++ test_fib.py
@@ -18,14 +18,14 @@ def test_negative_fibonacci():
 @pytest.mark.parametrize(
     "n, answer",
     [
-        (2, 1),
-        (3, 2),
-        (4, 3),
-        (5, 5),
-        (6, 8),
-        (7, 13),
-        (8, 21),
-        (9, 34),
+        pytest.param(2, 1, id="2"),
+        pytest.param(3, 2, id="3"),
+        pytest.param(4, 3, id="4"),
+        pytest.param(5, 5, id="5"),
+        pytest.param(6, 8, id="6"),
+        pytest.param(7, 13, id="7"),
+        pytest.param(8, 21, id="8"),
+        pytest.param(9, 34, id="9"),
     ]
 )
 def test_fib_various(n, answer):
