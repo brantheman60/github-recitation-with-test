3b9e886ca816eaa9d6cca8f2e0b49dff2d2552de
Kian Nassre <55859058+knassre-cmu@users.noreply.github.com>
2022-08-31 13:13:25 -0400

    Fix fibonacci(negative) (#4)
    * Extra tests
    
    * Fixed negatives
    
    * Renaming tests
    
    * Made style revisions
    
    Co-authored-by: Kian Nassre <knassre@macbook-pro-132.wifi.local.cmu.edu>

--- test_fib.py
+++ test_fib.py
@@ -1,4 +1,5 @@
 from fib import fibonacci
+import pytest
 
 # Test cases for the fibonacci function
 
@@ -13,3 +14,19 @@ def test_21st_fibonacci():
 
 def test_negative_fibonacci():
     assert(fibonacci(-1) == None)
+
+@pytest.mark.parametrize(
+    "n, answer",
+    [
+        pytest.param(2, 1, id="2"),
+        pytest.param(3, 2, id="3"),
+        pytest.param(4, 3, id="4"),
+        pytest.param(5, 5, id="5"),
+        pytest.param(6, 8, id="6"),
+        pytest.param(7, 13, id="7"),
+        pytest.param(8, 21, id="8"),
+        pytest.param(9, 34, id="9"),
+    ]
+)
+def test_fib_various(n, answer):
+    assert(fibonacci(n) == answer)
